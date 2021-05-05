e7d71300a5fdfcf3537eeb36b853cd90d9cf8b62
Александр Белов <belov_a@tochka.com>
2021-05-05 16:11:08 +0300

    style fixes

--- Bespereboiniki/Controllers/AccountController.cs
+++ Bespereboiniki/Controllers/AccountController.cs
@@ -13,6 +13,7 @@ namespace Bespereboiniki.Controllers
     public class AccountController : Controller
     {
         private readonly IUserRepository userRepository;
+        private readonly string homePage = "/";
 
         public AccountController(IUserRepository userRepository)
         {
@@ -21,7 +22,7 @@ namespace Bespereboiniki.Controllers
 
         public IActionResult Login([FromQuery] string returnUrl)
         {
-            return View(new UserModel() {ReturnUrl = returnUrl});
+            return View(new UserModel() {ReturnUrl = returnUrl ?? homePage});
         }
 
         [HttpPost]
@@ -32,11 +33,12 @@ namespace Bespereboiniki.Controllers
             if (user == null || user.Password != userModel.Password) return Redirect("/Account/AccessDenied");
 
             await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, CreateClaims(user));
-            return Redirect(userModel.ReturnUrl ?? "/");
+            return Redirect(userModel.ReturnUrl ?? homePage);
         }
 
-        public IActionResult AccessDenied()
+        public IActionResult AccessDenied([FromQuery] string returnUrl)
         {
+            ViewData["returnUrl"] = returnUrl ?? homePage;
             return View();
         }
 
@@ -56,30 +58,6 @@ namespace Bespereboiniki.Controllers
 
             var claimsIdentity = new ClaimsIdentity(
                 claims, CookieAuthenticationDefaults.AuthenticationScheme);
-
-            // var authProperties = new AuthenticationProperties
-            // {
-            //     //AllowRefresh = <bool>,
-            //     // Refreshing the authentication session should be allowed.
-            //
-            //     //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
-            //     // The time at which the authentication ticket expires. A 
-            //     // value set here overrides the ExpireTimeSpan option of 
-            //     // CookieAuthenticationOptions set with AddCookie.
-            //
-            //     //IsPersistent = true,
-            //     // Whether the authentication session is persisted across 
-            //     // multiple requests. When used with cookies, controls
-            //     // whether the cookie's lifetime is absolute (matching the
-            //     // lifetime of the authentication ticket) or session-based.
-            //
-            //     //IssuedUtc = <DateTimeOffset>,
-            //     // The time at which the authentication ticket was issued.
-            //
-            //     //RedirectUri = <string>
-            //     // The full path or absolute URI to be used as an http 
-            //     // redirect response value.
-            // };
 
             return new ClaimsPrincipal(claimsIdentity);
         }
