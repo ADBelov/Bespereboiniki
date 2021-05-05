89d30cf76695ad93612a95e4d3012cf689df782b
Александр Белов <belov_a@tochka.com>
2021-05-05 16:22:30 +0300

    seed method

--- Bespereboiniki.Datalayer/UPSContext.cs
+++ Bespereboiniki.Datalayer/UPSContext.cs
@@ -1,3 +1,4 @@
+using System;
 using Bespereboiniki.Datalayer.Tables;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,14 +12,46 @@ namespace Bespereboiniki.Datalayer
 
         public UPSContext()
         {
-            
         }
-        
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-           
+            
+            modelBuilder.Entity<Role>().HasData(new Role
+            {
+                Id = new Guid("B29F65E7-43B9-4A14-9AB2-DDCA2312F2E3"),
+                Name = "admin"
+            });
+
+            modelBuilder.Entity<Role>().HasData(new Role
+            {
+                Id = new Guid("57EB14FB-CFB9-43B6-869D-28BB06E57540"),
+                Name = "manager"
+            });
+
+            modelBuilder.Entity<User>().HasData(new User
+            {
+                Id = new Guid("869AB7AC-227D-4F45-B6B7-30455BD86A83"),
+                Login = "admin",
+                Password = "admin",
+                Surname = "Админович",
+                FirstName = "Админ",
+                LastName = "Фсея Руси",
+                RoleId = new Guid("B29F65E7-43B9-4A14-9AB2-DDCA2312F2E3")
+            });
+
+            modelBuilder.Entity<User>().HasData(new User
+            {
+                Id = new Guid("1AF8D4A8-0577-4F7B-A917-2083CF3590D7"),
+                Login = "manager",
+                Password = "manager",
+                Surname = "Менеджерович",
+                FirstName = "Менеджер",
+                LastName = "Фсея Бесперебойники.рф",
+                RoleId = new Guid("57EB14FB-CFB9-43B6-869D-28BB06E57540")
+            });
         }
-        
+
         // for migrations
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
